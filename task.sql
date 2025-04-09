50a27be7ae845e652a8258955a5ae7ddac2ddef1
Kateryna <kateryna.kotyh@gmail.com>
2025-04-09 17:49:49 +0300

    Updated

--- task.sql
+++ task.sql
@@ -1,6 +1,6 @@
 -- write your code for database user creation here
 CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-GRANT SELECT, INSERT, UPDATE, DELETE ON webapp.* TO 'webappuser'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON 'ShopBD'.* TO 'webappuser'@'%';
 
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-GRANT ALL PRIVILEGES ON 'deployment'.* TO 'deploymentuser'@'%';
+GRANT ALL PRIVILEGES ON 'ShopBD'.* TO 'deploymentuser'@'%';

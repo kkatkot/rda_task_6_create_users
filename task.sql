d41789a75a6afcbf4dfce66f0730b77617702a23
Kateryna <kateryna.kotyh@gmail.com>
2025-04-09 17:41:41 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,1 +1,6 @@
 -- write your code for database user creation here
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT SELECT, INSERT, UPDATE, DELETE ON webapp.* TO 'webappuser'@'%';
+
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL PRIVILEGES ON 'deployment'.* TO 'deploymentuser'@'%';
